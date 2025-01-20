8828e20d05ef78c6e4d89104bcf9ccaa0b108f22
ROSTAND LELE <rostoinfocus@gmail.com>
2025-01-20 22:26:06 +0100

    dfsd
    dvsd

---
+++ script.js
@@ -0,0 +1,83 @@
+// let nbre=2.5;
+// console.log(typeof(nbre));
+
+//  #Hoisting (Variable)
+
+// console.log(nbre1);
+// let nbre1 = 10;
+
+//  #Hoisting (Fonction)
+
+// console.log(sayHello());
+// function sayHello(){
+//   return "Hello";
+// }
+// const sayHello2= () =>{
+//   return "Hello";
+// }
+// console.log(sayHello2());
+
+// let a =12
+// console.log(a);
+// a =8
+// console.log(a);
+// let resutl = 0/0;
+// console.log(resutl);
+
+// let value =parseInt('abc');
+// console.log(value);
+
+
+// let som =Math.sqrt(-(-1))
+// console.log(isNaN(som));
+
+// let fruits =['🍏','🍌','🍇' , true, 25, 'Eva'];
+// console.log(fruits);
+// console.log(typeof(fruits));
+
+
+
+// let fruits2 =['Apple','Banana','grape'];
+// console.log(fruits2[0]);
+
+
+// let apple1 = "Apple🍎"
+// let banana1 = "Banana🍌"
+// let grape1 = "grape🍇"
+
+// console.log(apple1);
+let fruits =['🍏','🍌','🍇' ];
+
+const [apple, banana, grape] = fruits;
+// console.log(apple);
+// console.log(banana);
+// console.log(grape);
+
+const [apple1, ...rest ] = fruits;
+
+console.log(rest);
+console.log(apple1);
+
+let fruits4 =['🍏','🍌','🍇' ];
+let fruits5 =['🍏','🍌','🍇' ];
+ console.log(fruits4===fruits5);
+ 
+
+
+
+
+
+
+
+
+
+// let nbre2;
+// console.log(nbre2);
+
+// let nbre3=2;
+// console.log(nbre3);
+// // let nbre1 ;
+
+
+
+
